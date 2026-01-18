87b9b82545b434f705a885cff3e41cf0f67eba69
indra yukki <letscodingindra@gmail.com>
2026-01-18 19:34:41 +0700

    menambahkan dynamic

--- app/api/material-bom/export/route.ts
+++ app/api/material-bom/export/route.ts
@@ -1,6 +1,8 @@
 import { getPool } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   try {
     const pool = await getPool();
